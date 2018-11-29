d70582bc0577142f6a00b7ab475e539241d92b8c
Kahaan <kahaan.r.patel@gmail.com>
2018-11-29 15:58:28 -0800

    init

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {count: 0}
+
+  }
+
+  click(event){
+    event.preventDefault()
+    this.setState({count: this.state.count + 1})
+  }
+
+  reset(event){
+    event.preventDefault()
+    this.setState({count: 0})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.count} <br />
+      <button onClick={this.click.bind(this)}>+</button>
+      <button onClick={this.reset.bind(this)}>Reset</button>
       </div>
     );
   }
