95de7dce2950fabb80e2340489c32423f8c8c0e4
Kahaan <kahaan.r.patel@gmail.com>
2018-11-29 16:14:25 -0800

    revision

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {count: 0}
+    this.state = {count: 0, previous:[]}
 
   }
 
@@ -16,15 +16,22 @@ class App extends Component {
 
   reset(event){
     event.preventDefault()
-    this.setState({count: 0})
+    let previous = this.state.previous
+    previous.push(this.state.count)
+    this.setState({count: 0, previous})
   }
 
   render() {
     return (
       <div className="App">
         {this.state.count} <br />
-      <button onClick={this.click.bind(this)}>+</button>
-      <button onClick={this.reset.bind(this)}>Reset</button>
+        <button onClick={this.click.bind(this)}>+</button>
+        <button onClick={this.reset.bind(this)}>Reset</button>
+        <ul>
+          {
+            this.state.previous.map(count => <li>{count}</li>)
+          }
+        </ul>
       </div>
     );
   }
